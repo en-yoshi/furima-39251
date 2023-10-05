d42b4d2b61206048a0e2eac157a7895fbc323d48
en-yoshi <endo.y.1989@gmail.com>
2023-10-05 19:47:23 +0900

    商品の編集・削除・購入画面に進む ボタンに影
    ログイン後、投稿商品を選択して出品者と購入者状況によりより表示されるボタンにカーソルを合わせると影が表示される。
    ※今のままだとボタンの範囲に問題

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("../item_price");
 require("../card")
 require("../preview")
+require("../item-box")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
